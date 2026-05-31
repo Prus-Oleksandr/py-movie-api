1d95d5022a96d7151ef368466ed1de891d4f3c41
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-05-31 11:30:10 +0300

    Implement serializers and api views

---
+++ cinema/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    duration = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Movies'
+
+    def __str__(self):
+        return self.title
